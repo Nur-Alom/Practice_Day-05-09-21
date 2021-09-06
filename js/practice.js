3fbe16609c49b3b997f9e5ec80da81bf8cda4416
Nur-Alom <alamn7150@gmail.com>
2021-09-06 15:05:29 +0600

    first commit

---
+++ js/practice.js
@@ -0,0 +1,39 @@
+
+// 1. task
+
+// const doSomething = () => {
+//     console.log('iam coding javaScript');
+// }
+
+
+// console.log('iam the first');
+// console.log('iam the second');
+// console.log('iam the third');
+// setTimeout(doSomething, 3500);
+// console.log('iam the fourth');
+// console.log('iam the fifth');
+// console.log('iam the sixth');
+
+// 2. task
+
+// const sum = () => {
+//     const promptInput = prompt('please inter your amount');
+//     const inputValue = parseFloat(promptInput);
+//     if (promptInput === '') {
+//         alert('please inter a number!');
+//     }
+//     else if (promptInput >= 0) {
+//         const add = inputValue + 200;
+//         alert(add);
+//     }
+// }
+
+// 3. task
+
+// const confirmation = () => {
+//     const askQ = confirm('can you See Your Browser Location?');
+//     if (askQ === true) {
+//         console.log('http://127.0.0.1:5500/index.html')
+//     }
+// }
+
